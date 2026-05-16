691d189b8bdb0fe0dd2554d4606d61afcafbc2fe
Aurellia Azzahra Kacaribu <aureliazhra@gmail.com>
2026-05-16 11:39:47 +0700

    Create Player.java

---
+++ MODUL 11/Player.java
@@ -0,0 +1,14 @@
+public class Player {
+    int tinggi;
+    int berat;
+
+    public Player(int tinggi, int berat) {
+        this.tinggi = tinggi;
+        this.berat = berat;
+    }
+
+    @Override
+    public String toString() {
+        return "Tinggi: " + tinggi + " cm, Berat: " + berat + " kg";
+    }
+}
